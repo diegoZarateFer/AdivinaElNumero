ca0ef3ad4f892e3eae2e518058dfda7e35a85c20
Diego Zárate <diegozaratefernandez300@gmail.com>
2025-05-24 18:39:00 -0600

    Implementando la lógica para almacenar y mostrar el historial de juegos.

--- lib/widgets/pantalla_principal.dart
+++ lib/widgets/pantalla_principal.dart
@@ -5,6 +5,7 @@ import 'package:adivinaelnumero/widgets/contenedor_numeros.dart';
 import 'package:adivinaelnumero/widgets/historial.dart';
 import 'package:adivinaelnumero/widgets/selector_niveles.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PantallaPrincipal extends StatefulWidget {
   const PantallaPrincipal({super.key});
@@ -18,11 +19,12 @@ class PantallaPrincipal extends StatefulWidget {
 class _PantallaPrincipalState extends State<PantallaPrincipal> {
   final _formKey = GlobalKey<FormState>();
 
+  bool _cargandoHistorial = true;
   final TextEditingController _controladorNumero = TextEditingController();
   int _indiceNivelSeleccionado = 0;
   int _intentosRestantes = 5;
   int _numeroEscondido = Random().nextInt(10) + 1;
-  List<int> _mayorQue = [], _menorQue = [];
+  List<int> _mayorQue = [], _menorQue = [], _historial = [];
 
   int _generarNumeroEscondido(int maxNumero) {
     return Random().nextInt(maxNumero) + 1;
@@ -83,6 +85,12 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
       int numeroIngresado = int.parse(_controladorNumero.text.toString());
       if (numeroIngresado == _numeroEscondido) {
         // Se ha adivinado el número
+
+        setState(() {
+          _historial.add(_numeroEscondido);
+        });
+        _actualizarHistorial();
+
         ScaffoldMessenger.of(context).clearSnackBars();
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
@@ -107,13 +115,19 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
 
       // Se terminan los intentos disponibles.
       if (_intentosRestantes == 0) {
-        _generarNuevoNivel(_indiceNivelSeleccionado);
+        setState(() {
+          _historial.add(-_numeroEscondido);
+        });
+        _actualizarHistorial();
+
         ScaffoldMessenger.of(context).clearSnackBars();
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text('Se terminaron los intentos. Has perdido.'),
           ),
         );
+
+        _generarNuevoNivel(_indiceNivelSeleccionado);
       }
 
       _controladorNumero.clear();
@@ -135,95 +149,123 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
     _formKey.currentState!.validate();
   }
 
+  Future<void> _cargarHistorial() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // Extrayendo historial de shared preferences.
+    final historialObtenido = prefs.getStringList('historial') ?? [];
+    _historial = historialObtenido.map((s) => int.parse(s)).toList();
+    setState(() {
+      _cargandoHistorial = false;
+    });
+  }
+
+  Future<void> _actualizarHistorial() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final historialAGuardar = _historial.map((x) => x.toString()).toList();
+    prefs.setStringList('historial', historialAGuardar);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarHistorial();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Adivina el Número'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.menu),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 180,
-                    child: Form(
-                      key: _formKey,
-                      child: TextFormField(
-                        keyboardType: TextInputType.numberWithOptions(),
-                        maxLength: 4,
-                        controller: _controladorNumero,
-                        validator: _validarEntradaUsuario,
-                        onEditingComplete: _gestionarIntento,
-                        decoration: InputDecoration(
-                          label: Text('Numero'),
-                          border: OutlineInputBorder(),
-                          hintText: '####',
-                          counterText: "",
+    return _cargandoHistorial
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Adivina el Número'),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.menu),
+                ),
+              ],
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 180,
+                          child: Form(
+                            key: _formKey,
+                            child: TextFormField(
+                              keyboardType: TextInputType.numberWithOptions(),
+                              maxLength: 4,
+                              controller: _controladorNumero,
+                              validator: _validarEntradaUsuario,
+                              onEditingComplete: _gestionarIntento,
+                              decoration: InputDecoration(
+                                label: Text('Numero'),
+                                border: OutlineInputBorder(),
+                                hintText: '####',
+                                counterText: "",
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        ContadorIntentos(
+                          intentosRestantes: _intentosRestantes,
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    width: 32,
-                  ),
-                  ContadorIntentos(
-                    intentosRestantes: _intentosRestantes,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ContenedorNumeros(
-                      titulo: 'Mayor que',
-                      numeros: _mayorQue,
+                    const SizedBox(
+                      height: 16,
                     ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                    child: ContenedorNumeros(
-                      titulo: 'Menor que',
-                      numeros: _menorQue,
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ContenedorNumeros(
+                            titulo: 'Mayor que',
+                            numeros: _mayorQue,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: ContenedorNumeros(
+                            titulo: 'Menor que',
+                            numeros: _menorQue,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Historial(
+                            juegos: _historial,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                    child: Historial(
-                      juegos: [],
+                    const SizedBox(
+                      height: 64,
                     ),
-                  ),
-                ],
+                    SelectorNiveles(
+                      valor: _indiceNivelSeleccionado.toDouble(),
+                      cambiarDeNivel: _generarNuevoNivel,
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 64,
-              ),
-              SelectorNiveles(
-                valor: _indiceNivelSeleccionado.toDouble(),
-                cambiarDeNivel: _generarNuevoNivel,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
